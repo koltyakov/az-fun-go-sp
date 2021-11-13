c3dac769b13fa993ce6e5a12bff5c244cfe182ec
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2021-11-13 13:48:33 +0300

    Infra

--- infra/01_main.tf
+++ infra/01_main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = ">= 2.81"
+      version = ">= 2.85"
     }
   }
   backend "azurerm" {}
@@ -10,7 +10,6 @@ terraform {
 
 provider "azurerm" {
   subscription_id = var.subscription_id
-
   features {}
 }
 
