8912fe00de87dafe0a0271b82752180a05575ff2
Andrew Koltyakov <andrew.koltyakov@gmail.com>
2022-09-01 02:33:53 +0400

    Misc

--- infra/01_main.tf
+++ infra/01_main.tf
@@ -27,7 +27,6 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   tags                = var.tags
-
 }
 
 resource "azurerm_subnet" "subnet" {
